fe0d59074efb0ec94a80c6238e48afcd29069206
KTMichael <kmpg1822@gmail.com>
2021-12-09 12:18:32 -0800

    able to login

--- Server/index.js
+++ Server/index.js
@@ -4,14 +4,12 @@ const app = express();
 const cors = require('cors');
 const morgan = require('morgan');
 const passport = require('passport');
-const session = require('express-session')
+const session = require('express-session');
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
 const router = require('./routes');
 const port = 3000;
 
-const { GOOGLE_CLIENT_ID,
-  GOOGLE_CLIENT_SECRET }
-
+const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = require('../config.js');
 
 app.use(cors());
 app.use(morgan('dev'));
@@ -20,76 +18,78 @@ app.use('/', router);
 
 //Middleware
 app.use(session({
-  secret: "secret",
+  secret: 'secret',
   resave: false,
   saveUninitialized: true,
-}))
+}));
 
-app.use(passport.initialize()) // init passport on every route call
-app.use(passport.session())    //allow passport to use "express-session"
+app.use(passport.initialize()); // init passport on every route call
+app.use(passport.session()); //allow passport to use 'express-session'
 
 
 authUser = (request, accessToken, refreshToken, profile, done) => {
+  // TODO: Create new user if user doesnt exsist
+  // Have google_id in schema = profile.id
   return done(null, profile);
 };
 
 
-//Use "GoogleStrategy" as the Authentication Strategy
+//Use 'GoogleStrategy' as the Authentication Strategy
 passport.use(new GoogleStrategy({
   clientID: GOOGLE_CLIENT_ID,
   clientSecret: GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/callback",
+  callbackURL: 'http://localhost:3000/auth/google/callback',
   passReqToCallback: true
 }, authUser));
 
 
 passport.serializeUser((user, done) => {
-  console.log(`\n--------> Serialize User:`)
-  console.log(user)
-  // The USER object is the "authenticated user" from the done() in authUser function.
-  // serializeUser() will attach this user to "req.session.passport.user.{user}", so that it is tied to the session object for each session.
+  console.log('\n--------> Serialize User:');
+  console.log(user);
+  // The USER object is the 'authenticated user' from the done() in authUser function.
+  // serializeUser() will attach this user to 'req.session.passport.user.{user}', so that it is tied to the session object for each session.
 
-  done(null, user)
-})
+  done(null, user);
+});
 
 
 passport.deserializeUser((user, done) => {
-  console.log("\n--------- Deserialized User:")
-  console.log(user)
+  console.log('\n--------- Deserialized User:');
+  console.log(user);
   // This is the {user} that was saved in req.session.passport.user.{user} in the serializationUser()
-  // deserializeUser will attach this {user} to the "req.user.{user}", so that it can be used anywhere in the App.
+  // deserializeUser will attach this {user} to the 'req.user.{user}', so that it can be used anywhere in the App.
 
-  done(null, user)
-})
+  done(null, user);
+});
 
 
 //Start the NODE JS server
-app.listen(3000, () => console.log(`Server started on port 3000...`))
+app.listen(3000, () => console.log('Server started on port 3000...'));
 
 
-//console.log() values of "req.session" and "req.user" so we can see what is happening during Google Authentication
-let count = 1
+//console.log() values of 'req.session' and 'req.user' so we can see what is happening during Google Authentication
+let count = 1;
 showlogs = (req, res, next) => {
-  console.log("\n==============================")
-  console.log(`------------>  ${count++}`)
+  console.log('\n==============================');
+  console.log(`------------>  ${count++}`);
 
-  console.log(`\n req.session.passport -------> `)
-  console.log(req.session.passport)
+  console.log('\n req.session.passport -------> ');
+  console.log(req.session.passport);
 
-  console.log(`\n req.user -------> `)
-  console.log(req.user)
+  console.log('\n req.user -------> ');
+  console.log(req.user);
 
-  console.log("\n Session and Cookie")
-  console.log(`req.session.id -------> ${req.session.id}`)
-  console.log(`req.session.cookie -------> `)
-  console.log(req.session.cookie)
+  console.log('\n Session and Cookie');
+  console.log(`req.session.id -------> ${req.session.id}`);
+  console.log('req.session.cookie -------> ');
+  console.log(req.session.cookie);
 
-  console.log("===========================================\n")
+  console.log('===========================================\n');
 
-  next()
-}
+  next();
+};
 
-app.use(showlogs)
+app.use(showlogs);
 
 
 app.get('/auth/google',
@@ -106,29 +106,32 @@ app.get('/auth/google/callback',
   }));
 
 //Define the Login Route
-app.get("/login", (req, res) => {
-  res.render("login.ejs")
-})
+app.get('/login', (req, res) => {
+  res.send({ data: 'login' }).status(200);
+  // res.render('login.ejs');
+});
 
 
 //Use the req.isAuthenticated() function to check if user is Authenticated
 checkAuthenticated = (req, res, next) => {
-  if (req.isAuthenticated()) { return next() }
-  res.redirect("/login")
-}
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login');
+};
 
-//Define the Protected Route, by using the "checkAuthenticated" function defined above as middleware
-app.get("/dashboard", checkAuthenticated, (req, res) => {
-  res.render("dashboard.ejs", { name: req.user.displayName })
-})
+//Define the Protected Route, by using the 'checkAuthenticated' function defined above as middleware
+// home/calendar
+app.get('/', checkAuthenticated, (req, res) => {
+  res.send(`Welcome to Cultivate ${req.user.displayName}`).status(200);
+  // res.render('dashboard.ejs', { name: req.user.displayName });
+});
 
 //Define the Logout
-app.post("/logout", (req, res) => {
-  req.logOut()
-  res.redirect("/login")
-  console.log(`-------> User Logged out`)
-})
-
-app.listen(port, () => {
-  console.log(`listening on port ${port}`);
+app.post('/logout', (req, res) => {
+  req.logOut();
+  res.redirect('/login');
+  console.log('-------> User Logged out');
 });
+
+// app.listen(port, () => {
+//   console.log(`listening on port ${port}`);
+// });
