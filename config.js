62b5d27870f17894aad7f268b26cb5b51171e257
joshayres <me@joshayres.tech>
2021-12-11 12:30:47 -0800

    config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+//from Google Developer Console
+
+const credentials = {
+  GOOGLE_CLIENT_ID: '524746697695-822bnl1g01tu3q95bobbgmie8j1qlntv.apps.googleusercontent.com',
+  GOOGLE_CLIENT_SECRET: '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'
+};
+
+module.exports = credentials;
